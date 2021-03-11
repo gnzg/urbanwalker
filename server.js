da33883d72ced34be4dc47fc335856582811fb1a
gnzg <24455612+gnzg@users.noreply.github.com>
2021-03-11 22:34:36 +0100

    ADD: jshint, environment variables

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
 var http = require('http');
 var events = require('events');
-
 var emitter = new events.EventEmitter();
+var path = require('path');
+
+path.resolve(process.cwd(), '.env');
+
+require('dotenv').config();
 
 function onRequest(request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('Hello world!');
-    response.end();
-};
+    try {
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.write('Hello world!');
+        response.end();
+    } catch (e) {
+        console.log('Error:', e);
+    }
+}
 
 http.createServer(onRequest).listen(1337, '0.0.0.0');
 
-
-
 emitter.on('logging', data => {
     console.log('Logging', data);
-})
+});
 
-emitter.emit('logging', "Hello");
+emitter.emit('logging', 'Hello');
+emitter.emit('logging', process.env.API_URI);
