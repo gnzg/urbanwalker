b08d6074c04e5eeaa5017808a83f8ef60d86c7af
gnzg <24455612+gnzg@users.noreply.github.com>
2021-03-12 01:45:40 +0100

    ADD: basic connectivity against Mongo Atlas DB

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ require("./server.js");
 const express = require("express");
 const mongoose = require("mongoose");
 
+const axios = require('axios');
+
 const PORT = process.env.PORT || 1337;
 const app = express();
 
@@ -17,16 +19,30 @@ async function start() {
             useNewUrlParser: true,
             useFindAndModify: false,
             useUnifiedTopology: true
-        });
+        })
+        .then(() => console.log("Database Connected Successfully"))
+        .catch(err => console.log(err));
+
         app.listen(PORT, () => {
-            console.log('Server has been started...');
+            console.log('Server is listening for requests...');
         });
-        const world = await World.find({});
-        if (world !== null) {
+
+
+        let world = await World.find({});
+
+        if (world.length > 0) {
             // Emitter calls
             emitter.emit('logging', 'Found a previously saved world.');
         } else {
-            emitter.emit('logging', 'No world generated yet. Generating a new world...');
+            emitter.emit('logging', 'No world generated yet.');
+            world = new World({name: "City 2021", time: "Midday", total_critters: 0 });
+            world.save().then((resolve, reject) => {
+                if (reject) {
+                    reject(new Error("Error generating world!"));
+                } else {
+                    console.log("Generating a new world...");
+                }
+            });
         }
     } catch (e) {
         console.log('Error:', e);
--- model/World.js
+++ model/World.js
@@ -1,10 +1,22 @@
+const { toInteger } = require('lodash');
 const { Schema, model } = require('mongoose');
 
 const schema = new Schema({
     name: {
         type: String,
         required: true
+    },
+    time: {
+        type: String,
+        require: true
+    },
+    total_critters: {
+        type: Number,
+        required: true
     }
+},
+{
+    collection: 'world'
 });
 
 module.exports = model('World', schema);
