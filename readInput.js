b1c89211d8194aeef9d16399432ed4d35bb44e34
gnzg <24455612+gnzg@users.noreply.github.com>
2021-03-13 20:52:40 +0100

    FIX: corrected conditional

--- readInput.js
+++ readInput.js
@@ -8,13 +8,19 @@ function readInput(question, currentEvent, callback = undefined) {
     }
 
     const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
+        input: process.stdin
     });
 
-    rl.question(question + " ", data => {
-        if (currentEvent.available_actions.indexOf(data) > 0) {
-            console.log("You decide to", data + ".");
+    rl.question(question + " ", input => {
+        
+        // readline trims whitespaces, so we need to temporarily replace them 
+        // with another character
+        let actions = currentEvent.available_actions;
+        let temporaryActions = actions.map(item => item.replace(/\s/g, '_'));
+        let temporaryInput = input.replace(/\s/g, '_');
+
+        if (temporaryActions.indexOf(temporaryInput) >= 0) {
+            console.log("You decide to", input + ".");
         }
         else {
             console.log("Invalid input!");
